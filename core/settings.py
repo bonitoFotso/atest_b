65779f2a6822627337d4f29eaa74790c99b19bc0
bonitoFotso <bonitofotso55@gmail.com>
2025-04-28 16:32:05 +0100

    ew

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret !
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY="VotreCléSetwetwewewetweecrèteIciiodhuofsduoygioysioydio"
 #DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
